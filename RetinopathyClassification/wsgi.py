4dcc5f6d69bfcac3aea185e3286b31a1ce88bec0
Govind S <govindsanthosh98@gmail.com>
2022-09-27 19:03:55 +0530

    Initial commit

---
+++ RetinopathyClassification/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for RetinopathyClassification project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.wsgi import get_wsgi_application
+path_home = str(Path(__file__).parents[1])
+if path_home not in sys.path:
+    sys.path.append(path_home)
+
+os.environ['DJANGO_SETTINGS_MODULE']= 'RetinopathyClassification.settings'
+
+application = get_wsgi_application()
