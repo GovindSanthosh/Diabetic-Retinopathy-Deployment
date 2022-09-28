0aa119bbde908eac4c388bc8890a1c56a0457b01
Govind S <govindsanthosh98@gmail.com>
2022-09-28 10:55:16 +0530

    trying to fix port issues with gunicorn

--- RetinopathyClassification/wsgi.py
+++ RetinopathyClassification/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
-path_home = str(Path(__file__).parents[1])
-if path_home not in sys.path:
-    sys.path.append(path_home)
 
-os.environ['DJANGO_SETTINGS_MODULE']= 'RetinopathyClassification.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings.local")
 
 application = get_wsgi_application()
